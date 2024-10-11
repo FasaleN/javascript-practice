f8ed5a81599f6530055ad229f9064988ec050076
Dmitry Sayko <gromov2300@gmail.com>
2024-10-11 14:44:26 +0300

    add sixth homework (6.3)

--- js/script.js
+++ js/script.js
@@ -26,3 +26,14 @@ function getSumOfNumbers(sumNumbers) {
 let sumOfNumbers = [1, 'hello', true, 5, 8, null, 'world', 10];
 let resultSum = getSumOfNumbers(sumOfNumbers);
 alert(resultSum);
+
+//Third Homework
+
+function removeElement(array, item){
+    const newArray = array.indexOf(item);
+    if (newArray !== -1) array.splice(newArray, 1);
+    return array;
+}
+const array = [1, 3, 4, 6, 2, 5, 7];
+removeElement(array,4);
+console.log(array);
