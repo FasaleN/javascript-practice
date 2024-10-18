62b9b62464805c0c7ddfeafdba13964a96887956
Dmitry Sayko <gromov2300@gmail.com>
2024-10-18 16:20:37 +0300

    add eighth homework

--- js/script.js
+++ js/script.js
@@ -1,39 +1,16 @@
-
-//First Homework
-
-function getSumNumbers(){
-    let sum = 0;
-    return function(randomNumbers){
-        sum += randomNumbers;
-        return sum;
-    };
-}
-const finalSum = getSumNumbers();
-console.log(finalSum(4));
-console.log(finalSum(6));
-console.log(finalSum(10));
-console.log(finalSum(45));
-
-//Second Homework
-
-function multiplySum(firstNumber){
-    return (secondNumber) => {
-        return firstNumber * secondNumber;
+const stepsLadder = {
+    count: 0,
+    doStepUp: function () {
+        this.count++;
+        return this;
+    },
+    doStepDown: function () {
+        this.count--;
+        return this;
+    },
+    getShowStep: function () {
+        alert(this.count);
+        return this;
     }
-}
-console.log(multiplySum(5)(2));
-
-//Third Homework
-
-function getRandomNumbers(){
-    let userNumber;
-    for(let i = 0; i < 10; i++) {
-        userNumber = +prompt('Введіть число більше 100')
-        if (userNumber > 100) {
-            console.log(`Введене число більше 100 ${userNumber}`);
-            return;
-        }
-        if (userNumber === null)break;
-    }
-}
-getRandomNumbers();
+};
+stepsLadder.doStepUp().doStepDown().doStepDown().getShowStep();
